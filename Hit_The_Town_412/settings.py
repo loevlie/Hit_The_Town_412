2707c7c2395b02b392ba7322e0c94e079eed20f9
loevliedenny@gmail.com <59749099+loevlie@users.noreply.github.com>
2021-06-09 21:34:58 -0400

    Got the bootstrap template working

--- Hit_The_Town_412/settings.py
+++ Hit_The_Town_412/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
@@ -137,3 +137,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
